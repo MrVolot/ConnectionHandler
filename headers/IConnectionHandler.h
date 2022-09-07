e878bdc6b22a6e9870ed33685edc11273d7f1423
Mr_Volot <kiril-kiril2003@ukr.net>
2022-09-07 19:56:18 +0300

    Ssl connection server side implementation

--- headers/IConnectionHandler.h
+++ headers/IConnectionHandler.h
@@ -2,6 +2,7 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <functional>
+#include <boost/asio/ssl.hpp>
 
 template <typename ConnectionClass>
 class IConnectionHandler {
@@ -11,7 +12,7 @@ public:
    virtual void callAsyncRead() = 0;
    virtual void callRead() = 0;
    virtual void callWrite(const std::string& str) = 0;
-   virtual boost::asio::ip::tcp::socket& getSocket() = 0;
+   virtual boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& getSocket() = 0;
    virtual std::unique_ptr<boost::asio::streambuf>& getStrBuf() = 0;
    virtual void setMutableBuffer() = 0;
    virtual ConnectionClass& getConnector() = 0;
