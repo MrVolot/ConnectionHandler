348cc540d54e5a34e455ee61d3a2ebaf51dd3a2d
Mr_Volot <kiril-kiril2003@ukr.net>
2022-11-09 20:43:02 +0200

    Changed read/write logic (added delimeter)

--- headers/ConnectionHandler.h
+++ headers/ConnectionHandler.h
@@ -8,11 +8,11 @@ class ConnectionHandler : public std::enable_shared_from_this<ConnectionHandler<
     boost::asio::io_service& service_;
     const size_t msgLength_{ 1024 };
     std::unique_ptr<boost::asio::streambuf> strBuf_;
-    boost::asio::streambuf::mutable_buffers_type mutableBuffer_;
     std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler<ConnectionClass>>, const boost::system::error_code&, size_t)> readCallback_;
     std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler<ConnectionClass>>, const boost::system::error_code&, size_t)> asyncReadCallback_;
     std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler<ConnectionClass>>, const boost::system::error_code&, size_t)> writeCallback_;
 	ConnectionClass& caller_;
+	static constexpr auto delimiter{ "\r\n\r\n" };
 public:
     ConnectionHandler(boost::asio::io_service& service, ConnectionClass& caller);
     ConnectionHandler(const ConnectionHandler& rhs) = delete;
@@ -30,7 +30,7 @@ public:
 
 template<typename T>
 ConnectionHandler<T>::ConnectionHandler(boost::asio::io_service& service, T& caller) : socket_{ service },
-service_{ service }, strBuf_{ new boost::asio::streambuf }, mutableBuffer_{ strBuf_->prepare(msgLength_) }, caller_{caller}
+service_{ service }, strBuf_{ new boost::asio::streambuf }, caller_{caller}
 {
 
 }
@@ -53,7 +53,7 @@ void ConnectionHandler<T>::setWriteCallback(std::function<void(T* obj, std::shar
 template<typename T>
 void ConnectionHandler<T>::callAsyncRead()
 {
-	socket_.async_receive(boost::asio::buffer(mutableBuffer_), boost::bind(ConnectionHandler<T>::asyncReadCallback_,
+	boost::asio::async_read_until(socket_, *strBuf_.get(), delimiter, boost::bind(ConnectionHandler<T>::asyncReadCallback_,
 		&caller_,
 		this->shared_from_this(),
 		boost::asio::placeholders::error,
@@ -62,7 +62,9 @@ void ConnectionHandler<T>::callAsyncRead()
 template<typename T>
 void ConnectionHandler<T>::callWrite(const std::string& str)
 {
-	socket_.async_write_some(boost::asio::buffer(str.c_str(), str.size()), boost::bind(ConnectionHandler<T>::writeCallback_,
+	std::string strToSend{ str };
+	strToSend.append(delimiter);
+	socket_.async_write_some(boost::asio::buffer(strToSend.c_str(), strToSend.size()), boost::bind(ConnectionHandler<T>::writeCallback_,
 		&caller_,
 		this->shared_from_this(),
 		boost::asio::placeholders::error,
@@ -81,7 +83,7 @@ std::unique_ptr<boost::asio::streambuf>& ConnectionHandler<T>::getStrBuf()
 template<typename T>
 void ConnectionHandler<T>::setMutableBuffer()
 {
-	mutableBuffer_ = strBuf_->prepare(msgLength_);
+	//mutableBuffer_ = strBuf_->prepare(msgLength_);
 }
 
 template<typename ConnectionClass>
@@ -93,5 +95,5 @@ ConnectionClass& ConnectionHandler<ConnectionClass>::getConnector()
 template<typename ConnectionClass>
 void ConnectionHandler<ConnectionClass>::callRead()
 {
-	socket_.read_some(boost::asio::buffer(mutableBuffer_));
+	//socket_.read_some(boost::asio::buffer(mutableBuffer_));
 }
