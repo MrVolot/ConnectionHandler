2f59ba0d2117b7d8155aa262f6f29729ad4652e0
Mr_Volot <kiril-kiril2003@ukr.net>
2022-11-09 22:40:57 +0200

    Main successful fix!!!!

--- headers/IConnectionHandler.h
+++ headers/IConnectionHandler.h
@@ -2,18 +2,17 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <functional>
-#include <boost/asio/ssl.hpp>
 
 template <typename ConnectionClass>
 class IConnectionHandler {
 public:
-   virtual void setAsyncReadCallback(std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler>, const boost::system::error_code&, size_t)> callback) = 0;
-   virtual void setWriteCallback(std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler>, const boost::system::error_code&, size_t)> callback) = 0;
-   virtual void callAsyncRead() = 0;
-   virtual void callRead() = 0;
-   virtual void callWrite(const std::string& str) = 0;
-   virtual boost::asio::ssl::stream<boost::asio::ip::tcp::socket>& getSocket() = 0;
-   virtual std::unique_ptr<boost::asio::streambuf>& getStrBuf() = 0;
-   virtual void setMutableBuffer() = 0;
-   virtual ConnectionClass& getConnector() = 0;
+	virtual void setAsyncReadCallback(std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler>, const boost::system::error_code&, size_t)> callback) = 0;
+	virtual void setWriteCallback(std::function <void(ConnectionClass* obj, std::shared_ptr<IConnectionHandler>, const boost::system::error_code&, size_t)> callback) = 0;
+	virtual void callAsyncRead() = 0;
+	virtual void callRead() = 0;
+	virtual void callWrite(const std::string& str) = 0;
+	virtual boost::asio::ip::tcp::socket& getSocket() = 0;
+	virtual std::unique_ptr<boost::asio::streambuf>& getStrBuf() = 0;
+	virtual void setMutableBuffer() = 0;
+	virtual ConnectionClass& getConnector() = 0;
 };
